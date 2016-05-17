dd83218145832681a6d9ce23ab0a01da1e73c37d
beaukams <kamstelecom@hotmail.com>
2016-05-17 12:20:07 +0000

    bus

--- web/position_bus.php
+++ web/position_bus.php
@@ -17,15 +17,25 @@
 
 		if($base != -1){
 			//rechercher tous les bus de la ligne
-			$req = "SELECT * FROM bus WHERE nom_ligne=10";
+			$req = "SELECT bus.id_bus,matricule_bus,nom_ligne,latitude,longitude,altitude,vitesse,ladate,heure FROM bus,positionBus WHERE nom_ligne=10 AND positionBus.id_positionBus=bus.position_courant";
 			$req = $base->prepare($req);
 			$req->execute();
 			$bus = $req->fetchall();
 			echo "<div>";
+			echo "<ol id=a_" . $i . ">";
 			for($i=0; $i<count($bus); $i++){
-				echo "<div>" . $bus[$i][1] . "</div>";
-				//echo "lol";
+				
+				echo "<li id=id_" . $i . ">" . $bus[$i][0] . "</li>";
+				echo "<li id=mat_" . $i . ">" . $bus[$i][1] . "</li>";
+				echo "<li id=ligne_" . $i . ">" . $bus[$i][2] . "</li>";
+				echo "<li id=lat_" . $i . ">" . $bus[$i][3] . "</li>";
+				echo "<li id=lng_" . $i . ">" . $bus[$i][4] . "</li>";
+				echo "<li id=alt_" . $i . ">" . $bus[$i][5] . "</li>";
+				echo "<li id=v_" . $i . ">" . $bus[$i][6] . "</li>";
+				echo "<li id=dat__" . $i . ">" . $bus[$i][7] . "</li>";
+				echo "<li id=heure_" . $i . ">" . $bus[$i][8] . "</li>";
 			}
+			echo "</ol>";
 			echo "</div>";
 		}
 	}
